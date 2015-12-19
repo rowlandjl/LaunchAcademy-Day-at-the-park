e9fdd0508d6a1c58c0de51ca870a1b502cd97cce
rowlandjl <rowlandjl@vcu.edu>
2015-12-18 19:43:37 -0500

    LaunchAcademy-Day-at-the-park

---
+++ day_at_the_park.rb
@@ -0,0 +1,21 @@
+puts "What size sundae did you get?"
+size = gets.chomp
+
+puts "Who screamed loudest on the ride?"
+loudest = gets.chomp
+
+puts "What color of cotton candy was the most delicious?"
+color = gets.chomp
+
+puts "How cold was the water in the wave pool?"
+cold = gets.chomp
+
+puts "Were your favorite rides crazy or calm?"
+rides = gets.chomp
+
+puts "Summary of my day at the park:"
+puts "I got a " + size + " sundae, and it was delicious!"
+puts loudest + " screamed the loudest on the ride!"
+puts "The " + color + " cotton candy was the best!"
+puts "The water in the wave pool was " + cold + "!"
+puts "My favorite rides were " + rides + "."
